5add36edf93c80ec60f68b779e123fb9490fae09
enesigneci <enes.igneci@outlook.com>
2018-10-25 08:15:17 +0300

    Some bugfixes

--- app/src/main/java/com/enesigneci/customcoloredtoastapp/MainActivity.java
+++ app/src/main/java/com/enesigneci/customcoloredtoastapp/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 
 import com.enesigneci.customcoloredtoastlibrary.CustomColoredToast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomColoredToast.makeCustomColoredToast(MainActivity.this,"Custom Toast",CustomColoredToast.LENGTH_LONG,R.color.colorPrimary);
+        CustomColoredToast.makeCustomColoredToast(MainActivity.this,"Custom Toast",CustomColoredToast.LENGTH_LONG,"#FF0000");
     }
 }
--- customcoloredtoastlibrary/src/main/java/com/enesigneci/customcoloredtoastlibrary/CustomColoredToast.java
+++ customcoloredtoastlibrary/src/main/java/com/enesigneci/customcoloredtoastlibrary/CustomColoredToast.java
@@ -1,15 +1,16 @@
 package com.enesigneci.customcoloredtoastlibrary;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.widget.Toast;
 
 public class CustomColoredToast {
     public static final int LENGTH_SHORT=0;
     public static final int LENGTH_LONG=1;
 
-    public static void makeCustomColoredToast(Context context, String text, int duration,int color){
+    public static void makeCustomColoredToast(Context context, String text, int duration,String backgroundColor){
         Toast toast = Toast.makeText(context,text,duration);
-        toast.getView().setBackgroundColor(color);
+        toast.getView().setBackgroundColor(Color.parseColor(backgroundColor));
         toast.show();
     }
 }
